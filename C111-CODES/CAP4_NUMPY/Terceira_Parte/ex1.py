1de90cca23b3003d2aae3ed621ce7cb1e55fe892
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-03-23 15:09:02 -0300

    Exercício 1 OK

---
+++ C111-CODES/CAP4_NUMPY/Terceira_Parte/ex1.py
@@ -0,0 +1,13 @@
+import numpy as np          # importação da biblioteca NumPy
+
+# carregando o arquivo ''
+arr = np.loadtxt('..\\space.csv', delimiter=';', dtype=str, encoding='utf-8')
+
+contagem_total = len(arr) - 1                                          # quantidade total de missões
+#                                                                        coloquei o -1 para não contar o cabeçalho
+contagem_sucesso = len(arr[np.char.find(arr, 'Success') >= 0])         # quantidade de missões de sucesso
+print(f"Quantidade total de missões: {contagem_total}")
+print(f"Quantidade de missões com sucesso: {contagem_sucesso}")
+
+# mostra a porcentagem de missões que foram um sucesso
+print(f"Porcentagem de sucesso: {((contagem_sucesso/contagem_total)*100):.2f}%")
