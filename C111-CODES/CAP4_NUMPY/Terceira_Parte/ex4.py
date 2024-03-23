0bee1b1520d5c9af6403879fc85e59c8f6e0c47c
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-03-23 16:58:12 -0300

    Adicionei a form completa de filtragem, sem separar em duas arrays

--- C111-CODES/CAP4_NUMPY/Terceira_Parte/ex4.py
+++ C111-CODES/CAP4_NUMPY/Terceira_Parte/ex4.py
@@ -4,6 +4,10 @@ import numpy as np          # importação da biblioteca NumPy
 arr = np.loadtxt('..\\space.csv', delimiter=';', dtype=str, encoding='utf-8')
 arr = np.delete(arr, 0, axis=0)     # deletando o cabeçalho da array
 
+# O comando abaixo entrega direto a resposta, mas ficou muito poluido de entender, por isso separei as missões da SpaceX
+# e depois filtrei as viagens mais caras
+# SpaceX = arr[np.char.find(arr[:, 1], 'SpaceX') >= 0][arr[np.char.find(arr[:, 1], 'SpaceX') >= 0][:, 6] == max(arr[np.char.find(arr[:, 1], 'SpaceX') >= 0][:, 6])]
+
 # Pega todas as missões da SpaceX
 missoes_SpaceX = arr[np.char.find(arr[:, 1], 'SpaceX') >= 0]
 
