2c8ade3024c2a1b9de0f19f447c42094fb953170
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-03-23 16:46:01 -0300

    Exercício 4 OK

---
+++ C111-CODES/CAP4_NUMPY/Terceira_Parte/ex4.py
@@ -0,0 +1,13 @@
+import numpy as np          # importação da biblioteca NumPy
+
+# carregando o arquivo '.csv' em uma array do numpy
+arr = np.loadtxt('..\\space.csv', delimiter=';', dtype=str, encoding='utf-8')
+arr = np.delete(arr, 0, axis=0)     # deletando o cabeçalho da array
+
+# Pega todas as missões da SpaceX
+missoes_SpaceX = arr[np.char.find(arr[:, 1], 'SpaceX') >= 0]
+
+# dentre as missões da SpaceX retorna as missões mais caras
+SpaceX_mais_cara = missoes_SpaceX[missoes_SpaceX[:, 6] == max(missoes_SpaceX[:, 6])]
+
+print(f"A(s) missão(ões) da SpaceX mais cara(s): \n{SpaceX_mais_cara}")
