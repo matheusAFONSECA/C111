9dd35d8ed92371aa77a85fdade1d4887f31f0199
Matheus Fonseca <henrique.matheus@gec.inatel.br>
2024-03-23 16:46:32 -0300

    Separei para pegar somente as colunas

--- C111-CODES/CAP4_NUMPY/Terceira_Parte/ex3.py
+++ C111-CODES/CAP4_NUMPY/Terceira_Parte/ex3.py
@@ -4,6 +4,6 @@ import numpy as np          # importação da biblioteca NumPy
 arr = np.loadtxt('..\\space.csv', delimiter=';', dtype=str, encoding='utf-8')
 arr = np.delete(arr, 0, axis=0)     # deletando o cabeçalho da array
 
-contagem_usa = len(arr[np.char.find(arr, 'USA') >= 0])         # quantidade de missões nos USA
+contagem_usa = len(arr[np.char.find(arr[:, 2], 'USA') >= 0])         # quantidade de missões nos USA
 
 print(f"Quantidade de missões realizadas nos USA: {contagem_usa}")
